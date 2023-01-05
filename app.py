3893778d7213cdce07071f33892c3f05c9b5d462
ali farvardin <mohammadali@duck.com>
2023-01-06 00:48:20 +0330

    read deleted

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from time import sleep
 import Read_DB
 from flask import Flask , render_template
 import fipiran
+import json
+
+
+
 app=Flask(__name__)
 @app.route("/")
 def crawling():
@@ -13,29 +17,51 @@ def crawling():
 # reading DB file
 
 #using Read_DB and reader method
-    sleep(30)
+    sleep(50)
 
 
-    info_dict=Read_DB.reader()
-    names=info_dict.get('name')
-    stock=info_dict.get('stock')
-    deposit=info_dict.get('deposit')
-    bond=info_dict.get('bond')
-    other=info_dict.get('other')
-    cash=info_dict.get('cash')
+    # info_dict=Read_DB.reader()
+    # names=info_dict.get('name')
+    # stock=info_dict.get('stock')
+    # deposit=info_dict.get('deposit')
+    # bond=info_dict.get('bond')
+    # other=info_dict.get('other')
+    # cash=info_dict.get('cash')
     # table_info=[names,stock,deposit,bond,other,cash]
+    with open('/home/d4rk/Documents/funds/static/DB/DB.json','rb') as f:
+        data=json.load(f)
+    for _ in data:
+        name=[_.get('name') for _ in data]
+        stock=[_.get('stock') for _ in data]
+        deposit= [_.get('deposit') for _ in data]
+        bond= [_.get('bond') for _ in data]
+        other= [_.get('other') for _ in data]
+        cash= [_.get('cash') for _ in data]
 
+    
+    info_dict= {
+        "name":name,
+        "stock":stock,
+        "deposit":deposit,
+        "bond":bond,
+        "other":other,
+        "cash":cash,
+
+    }
 
     return render_template('index.html',
-    names=names,
-    stock=stock,
-    deposit=deposit,
-    bond=bond,
-    other=other,
-    cash=cash,
+    names=info_dict.get("names"),
+    stock=info_dict.get("stock"),
+    deposit=info_dict.get("deposit"),
+    bond=info_dict.get("bond"),
+    other=info_dict.get("other"),
+    cash=info_dict.get("cash"),
     )
 if __name__=="__main__":
-    app.run(debug=True,host="127.0.0.127",port="8585")
+    app.run(debug=True,
+    host="127.0.0.1",
+    port="8585"
+    )
 
 
 
--- fipiran.py
+++ fipiran.py
@@ -1,21 +1,24 @@
 from twisted.internet import reactor
 import scrapy
 from scrapy.crawler import CrawlerRunner
-from scrapy.utils.log import configure_logging
-from scrapy.crawler import CrawlerProcess
+# from scrapy.utils.log import configure_logging
+# from scrapy.crawler import CrawlerProcess
 import regs
 import pandas as pd
 from json import loads
 
 class fipiran(scrapy.Spider):
-    # custom_settings={
-    #     'FEEDS':{'static/DB/DB.json':{'format': 'json',
-    #         'encoding': 'utf8',
-    #             'store_empty': False, 'fields': None, 'indent': 4,}}}
+    custom_settings={
+        'FEEDS':{'static/DB/DB.json':{'format': 'json',
+            'encoding': 'utf8',
+            'overwrite': True,
+            'store_empty' : True,
+            }}}
     name="fipiran"
     start_urls=[regs.url+f"{_}" for _ in regs.regs]   
     def parse(self, response):
-        df={
+        
+        yield {
             'name': ((loads(response.text).get('item')).get('name')),
             'stock': (loads(response.text).get('item')).get('stock'),
             'deposit': (loads(response.text).get('item')).get('deposit'),
@@ -24,10 +27,8 @@ class fipiran(scrapy.Spider):
             'other':((loads(response.text).get('item')).get('other')),
             'cash': ((loads(response.text).get('item')).get('cash')),
          }
-        
-        with open("static/DB/DB.json", "wb",encoding="utf-8") as file:
-            file.write(str(df))
 
+        
 runner = CrawlerRunner()
 d = runner.crawl(fipiran)
 d.addBoth(lambda _: reactor.stop())
